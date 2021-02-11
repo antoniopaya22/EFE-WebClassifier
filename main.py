57bccfb2cd967ff915af89ec2df254e0c8b5f67d
Antonio Paya <antonioalfa22@gmail.com>
2021-02-11 19:58:46 +0100

    Crawler v1.0

--- main.py
+++ main.py
@@ -4,10 +4,8 @@ from test.test_crawler import main, parse_args
 def test_crawler():
     """Test crawler
 
-    Ejemplo recorrido en profundidad:
-        python main.py --file links.txt --name prueba --sec 100 --mx 50 -p
     Ejemplo recorrido en anchura:
-        python main.py --file links.txt --name prueba --sec 100 --mx 50 -a
+        python main.py --name prueba --sec 10 --mx 50
     """
     main(parse_args())
 
