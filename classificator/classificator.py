9525a1f363891b96d36fc37166da17a03def813c
albact7 <alba164@gmail.com>
2021-02-12 18:41:17 +0100

    Fixed classifier output

--- classificator/classificator.py
+++ classificator/classificator.py
@@ -67,24 +67,25 @@ def list_to_string(l):
     return string
 
 def classify_all_pages(pages):
+    result = {}
     for page in pages.keys():
         content = list_to_string(pages[page])
         (cat, subcat) =  get_page_category([content])
-        print(page)
-        print("\tCategory is: ", cat)
-        print("\tSubcategory is: ", subcat)
+        result[page] = [cat, subcat]
+    return result
+ 
 
+if __name__ == '__main__':
 
-#(cat, subcat) =  get_page_category(['500 sheets standard copy paper toner ink'])
-#(cat, subcat) =  get_page_category(['Samsung 960 EVO 500 GB PCIe NVMe M.2 (2280) Internal Solid State Drive (SSD) (MZ-V7S500)'])
-#(cat, subcat) =  get_page_category(['Team Group 16 GB USB stick'])
-#(cat, subcat) =  get_page_category(['* WINTER SALE! * Acer Aspire 5 N19H2, Intel i5 10210U Processor, 8GB RAM, 256GB SSD, 14" IPS Thin Bezel FHD Display, Windows 10, 1 Year Warranty. Packed with the latest and greatest components. Making this laptop a top performer while still being extremely energy efficient thanks to its 10th generation Intel Processor. This laptop gives you all the features you could possibly want for a daily driver machine, see specs below for more info:'+
-#'Intel i5 10210U Processor 8GB DDR4 RAM, 256GB SSD, 14" IPS Thin Bezel FHD Display, Ultra Fast WIFI Connection, Long Life Battery Up To 12.5 Hours, USB Type-C x 1, USB 3.1 x 2, USB 2.0 x 1'])
-data = ''
-json_file = sys.argv[1]
-print(json_file)
-with open(json_file) as f:
-  data = json.load(f)
-  classify_all_pages(data)
+    #(cat, subcat) =  get_page_category(['500 sheets standard copy paper toner ink'])
+    #(cat, subcat) =  get_page_category(['Samsung 960 EVO 500 GB PCIe NVMe M.2 (2280) Internal Solid State Drive (SSD) (MZ-V7S500)'])
+    #(cat, subcat) =  get_page_category(['Team Group 16 GB USB stick'])
+    #(cat, subcat) =  get_page_category(['* WINTER SALE! * Acer Aspire 5 N19H2, Intel i5 10210U Processor, 8GB RAM, 256GB SSD, 14" IPS Thin Bezel FHD Display, Windows 10, 1 Year Warranty. Packed with the latest and greatest components. Making this laptop a top performer while still being extremely energy efficient thanks to its 10th generation Intel Processor. This laptop gives you all the features you could possibly want for a daily driver machine, see specs below for more info:'+
+    #'Intel i5 10210U Processor 8GB DDR4 RAM, 256GB SSD, 14" IPS Thin Bezel FHD Display, Ultra Fast WIFI Connection, Long Life Battery Up To 12.5 Hours, USB Type-C x 1, USB 3.1 x 2, USB 2.0 x 1'])
+    data = ''
+    json_file = sys.argv[1]
+    f = open(json_file, 'r')
+    data = f.read()
+    print(classify_all_pages(eval(data)))
 
 
